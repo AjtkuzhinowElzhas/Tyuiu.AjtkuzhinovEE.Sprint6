3c81247e115663c8785d9ef2fa7ce4d5a264ce04
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-12-04 23:26:06 +0300

    Task0

---
+++ Tyuiu.AjtkuzhinovEE.Sprint6.Task0.V8.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint6.Task0.V8.Lib
+{
+    public class DataService : ISprint6Task0V8
+    {
+        public double Calculate(int x)
+        {
+            
+            double y = (2 * x * x - 1) / Math.Pow(x * x - 2, 0.5);
+            return Math.Round(y,3);
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint6.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint6.Task0.V8.Lib;
+namespace Tyuiu.AjtkuzhinovEE.Sprint6.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            var ds= new DataService();
+            int x = 3;
+            double wait = 6.425;
+            double result = ds.Calculate(x);
+            Assert.AreEqual(wait, result);
+
+        }
+    }
+}
