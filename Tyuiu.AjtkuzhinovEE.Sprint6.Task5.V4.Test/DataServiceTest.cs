505ad6877385792beeb9a6d7dfd5307d61737a2e
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-12-12 18:15:40 +0300

    Task5

---
+++ Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[] paths = { @"C:\Users\user\source\repos\Tyuiu.AjtkuzhinovEE.Sprint6\Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4\bin\Debug\net8.0-windows\InPutDataFileTask5V4.txt" };
+            string fullPath = Path.Combine(paths);
+
+            FileInfo fileInfo = new FileInfo(fullPath);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4/FormMain.cs
@@ -0,0 +1,12 @@
+using Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint6.Task5.V4
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
